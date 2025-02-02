5593a783a61cc1c27cf5996325357a45309814d6
Brolindo <brolinufmg@gmail.com>
2025-02-02 16:50:19 -0300

    Arquivo realocado em outra pasta

--- Ex02.py
+++
@@ -1,9 +0,0 @@
-#Primeiro, execute a atribuição palavras =
-#['taco', 'bola', 'celeiro', 'cesta', 'peteca']
-#Agora, escreva duas expressões Python que são avaliadas, respectivamente,
-#como a primeira e a última palavras em palavras, na ordem do dicionário.
-
-palavras = ['taco', 'bola', 'celeiro', 'cesta', 'peteca']
-print(type(palavras))
-print(palavras[0])
-print(palavras[4])
